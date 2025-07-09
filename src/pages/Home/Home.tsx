a2ebe0a9f358cad64d2e03555533d1ddb77ba80b
priyanshivmehta <pvmehta112@gmail.com>
2025-07-09 17:48:43 +0530

    design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,14 +13,14 @@ const Home: React.FC = () => {
 
       {/* Hero Content (Right Center, text left-aligned) */}
       <div className="absolute inset-0 z-10 flex items-center justify-end px-10">
-        <div className="p-4 sm:p-6 md:p-10 rounded-xl max-w-xl w-full text-left text-black font-poppins">
+        <div className="p-4 sm:p-6 md:p-10 rounded-xl max-w-xl w-full text-left text-white font-poppins">
   <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold">
     Welcome to ChAild
   </h1>
-  <p className="text-sm sm:text-base md:text-lg lg:text-xl mt-4 drop-shadow text-black">
+  <p className="text-sm sm:text-base md:text-lg lg:text-xl mt-4 drop-shadow text-white">
     Guiding You Through the First 1000 Days
   </p>
-  <button className="mt-6 px-6 py-3 border-2 border-black text-black rounded-md text-lg font-medium hover:bg-[#ebc7ea] transition duration-300">
+  <button className="mt-6 px-6 py-3 border-2 border-white text-white rounded-md text-lg font-medium hover:bg-white hover:text-black transition duration-300">
     Get Started
   </button>
 </div>
